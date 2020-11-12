643fd73308195e2383ad8b5093def93cb482946e
'leena park' <'hrn444@naver.com'>
2020-11-12 17:17:16 +0900

    201112

--- src/com/javaex/ex03/EX08.java
+++ src/com/javaex/ex03/EX08.java
@@ -1,5 +1,42 @@
 package com.javaex.ex03;
 
-public class EX08 {
+import java.util.Scanner;
 
+public class EX08 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요");
+		System.out.print("숫자: ");
+		/*내가 짠 코드
+		int num = sc.nextInt();
+		
+		if(num>0) {
+			if(num%2==0) {
+			System.out.println("짝수");
+			}else {
+				System.out.println("홀수");
+			}
+		}else if(num<0) {
+			System.out.println("음수");
+		}else {
+			System.out.println("0");
+		}
+		*/
+		
+		int point = sc.nextInt();
+		
+		if(point>0) {
+			if(point%2==0) {//조건식은 트루 값을 넣는다
+				System.out.println("짝수");
+			}else {
+				System.out.println("홀수");
+			}
+		}else if(point<0) {
+			System.out.println("음수");
+		}else {
+			System.out.println("0");
+		}
+		
+		sc.close();
+	}
 }
