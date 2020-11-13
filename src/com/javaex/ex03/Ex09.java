469aee805c0f29046e862163cb46c50f64dc3b05
'leena park' <'hrn444@naver.com'>
2020-11-13 11:10:56 +0900

    201113

--- src/com/javaex/ex03/Ex09.java
+++ src/com/javaex/ex03/Ex09.java
@@ -10,7 +10,7 @@ public class Ex09 {
 		System.out.print("과목번호: ");
 		
 		int lecture = sc.nextInt();
-		
+		/*내가 짠 if문
 		if(lecture==1) {
 			System.out.println("R101호");
 		}else if(lecture==2) {
@@ -21,6 +21,27 @@ public class Ex09 {
 			System.out.println("R404호");
 		}else {
 			System.out.println("상담원에게 문의하세요");
+		}
+		*/
+		
+		// 스위치 케이스문
+		
+		switch(lecture){
+			case 1:
+				System.out.println("R101호");
+				break;
+			case 2:
+				System.out.println("R202호");
+				break;
+			case 3:
+				System.out.println("R303호");
+				break;
+			case 4:
+				System.out.println("R404호");
+				break;
+			default:
+				System.out.println("상담원과 문의하세요");
+				break;
 		}
 		
 		sc.close();
---
+++ src/com/javaex/ex03/Ex11.java
@@ -0,0 +1,32 @@
+package com.javaex.ex03;
+
+import java.util.Scanner;
+
+public class Ex11 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("월을 입력하세요");
+		
+		int month = sc.nextInt();
+		
+		switch(month) {
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				System.out.println("30일 입니다");
+				break;
+			case 2:
+				System.out.println("28일 입니다");
+				break;
+			default :
+				System.out.println("31일 입니다");
+				break;
+		}
+		
+		//switch 축소ver. 많이 사용되지는 않음 권장 x
+		
+		sc.close();
+	}
+
+}
